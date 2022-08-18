b7f7c687b855f76f487b284e40e6606f03cbf06d
gabrielcancro <gabrielcancro@3ogs.com.ar>
2022-08-17 23:23:30 -0300

    update

--- Prefabs/EndPanel.gd
+++ Prefabs/EndPanel.gd
@@ -32,6 +32,7 @@ func check_all_finished():
 		print("END ",p," ",GC.GAME.players[p].turn)
 		if(GC.GAME.players[p].turn<GC.GAME.max_turns): finished = false
 	if finished:
+		show_final_table()
 		$Label_win.text = get_node("../Interaction/ScoreTable").table[0].name
 		$Label_noFinish.text = ""
 
@@ -41,4 +42,35 @@ func update_stock():
 	if("civ_bonif" in GC.PLAYER): for bon in GC.PLAYER["civ_bonif"]: data[bon] += 1
 	for bon in $VBox_stock.get_children():
 		bon.get_node("Label").text = str( data[bon.name] )+"x"+str( GC.PLAYER[bon.name] )+"="+str( data[bon.name]*GC.PLAYER[bon.name] )
-	$Label_extra.text = str(GC.PLAYER.end_bonif_points)
+#	$Label_extra.text = str(GC.PLAYER.end_bonif_points)
+
+func show_final_table():
+	var table = []
+	for i in GC.GAME.players:
+		var pl = GC.GAME.players[i]
+		pl["bonif"] = _get_civ_bonif(pl)
+		pl["final_score"] = pl.score
+		pl["final_score"] += pl.villager*pl["bonif"].villager
+		pl["final_score"] += pl.camp*pl["bonif"].camp
+		pl["final_score"] += pl.tool*pl["bonif"].tool
+		pl["final_score"] += pl.build*pl["bonif"].build
+	
+	for label in $FinalTable/HBox.get_children():
+		label.text = ""
+	for i in GC.GAME.players.keys():
+		var pl = GC.GAME.players[i]
+		var bonif = pl["bonif"]
+		$FinalTable/HBox/lb_name.text += i
+		$FinalTable/HBox/lb_score.text += str(pl.score)
+		$FinalTable/HBox/lb_villager.text += str(pl.villager)+"x"+str(bonif.villager)
+		$FinalTable/HBox/lb_camp.text += str(pl.camp)+"x"+str(bonif.camp)
+		$FinalTable/HBox/lb_tool.text += str(pl.tool)+"x"+str(bonif.tool)
+		$FinalTable/HBox/lb_build.text += str(pl.build)+"x"+str(bonif.build)
+		$FinalTable/HBox/lb_looter.text += "-"
+		$FinalTable/HBox/lb_total.text += str(pl.final_score)
+
+func _get_civ_bonif(pl):
+	var bonif = {"villager":0,"tool":0,"camp":0,"build":0}
+	if !"civ_bonif" in pl: pl["civ_bonif"] = []
+	for i in pl["civ_bonif"]: bonif[i] += 1
+	return bonif
