954549f2ca0be4546372b403061324d834a8e609
gabrielcancro <gabrielcancro@3ogs.com.ar>
2022-08-30 09:24:49 -0300

    update

--- Games/Ludus/ConfigVars.gd
+++ Games/Ludus/ConfigVars.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var CONFIG_NAMES=["init_turns","phs_turns","duel_period","total_duels","duration"]
 func _ready():
 	pass
 
@@ -13,44 +14,30 @@ func setReadOnly(val=true):
 	check_config_errors()
 
 func set_data_from_game(game):
-	if !"init_turns" in game: game.init_turns = 0
-	$Grid/init_turns/LineEdit.text = str(game.init_turns)
-	if !"max_turns" in game: game.max_turns = 0
-	$Grid/total_turns/LineEdit.text = str(game.max_turns)
-	if !"turns_phs" in game: game.turns_phs = 0
-	$Grid/phs_turns/LineEdit.text = str(game.turns_phs)
-	if !"duration" in game: game.duration = 0
-	$Grid/duration/LineEdit.text = str(game.duration)
+	for nm in CONFIG_NAMES:
+		var le = get_node("Grid/"+nm+"/LineEdit")
+		if !nm in game: game[nm] = 0
+		le.text = str(game[nm])
 
 func get_config_data():
 	if !check_config_errors(): return null
-	return {
-		"init_turns": int($Grid/init_turns/LineEdit.text),
-		"max_turns": int($Grid/total_turns/LineEdit.text),
-		"turns_phs": int($Grid/phs_turns/LineEdit.text),
-		"duration": float($Grid/final_await/LineEdit.text),
-	}
+	var result = {}
+	for nm in CONFIG_NAMES:
+		var le = get_node("Grid/"+nm+"/LineEdit")
+		result[nm] = int(le.text)
+	return result
 
 func check_config_errors():
 	var correct = true
-	if !$Grid/init_turns/LineEdit.text.is_valid_integer():
-		$Grid/init_turns/LineEdit.text = ""
-		correct = false
-	if !$Grid/phs_turns/LineEdit.text.is_valid_integer():
-		$Grid/phs_turns/LineEdit.text = ""
-		correct = false
-	if !$Grid/total_turns/LineEdit.text.is_valid_integer():
-		$Grid/total_turns/LineEdit.text = ""
-		correct = false
-	if !$Grid/final_await/LineEdit.text.is_valid_float():
-		$Grid/final_await/LineEdit.text = ""
-		correct = false
+	for nm in CONFIG_NAMES:
+		var le = get_node("Grid/"+nm+"/LineEdit")
+		if !le.text.is_valid_integer(): 
+			le.text = ""
+			correct = false
 	if correct:
-		var max_turns = int($Grid/total_turns/LineEdit.text)
-		var init_turns = int($Grid/init_turns/LineEdit.text)
-		var turns_phs = int($Grid/phs_turns/LineEdit.text)
-		var hs_await = int($Grid/final_await/LineEdit.text)
-		var duration = ceil( (max_turns-init_turns) / turns_phs ) + hs_await
-		$Grid/duration/LineEdit.text = str(duration)
+		var total_turns = int($Grid/total_duels/LineEdit.text) * int($Grid/duel_period/LineEdit.text)
+		total_turns -= int($Grid/init_turns/LineEdit.text)
+		var total_hs = ceil( total_turns/int($Grid/phs_turns/LineEdit.text) )
+		$Grid/duration/LineEdit.text = str(total_hs)
 	else: $Grid/duration/LineEdit.text = "?"
 	return correct
