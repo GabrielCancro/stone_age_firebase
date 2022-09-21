6ce0555550940ab572a1cccb284d9a78f51c5041
Gabriel Cancro <gabrielcancro@3ogs.com.ar>
2022-09-21 18:01:43 -0300

    update

--- clock_service.gd
+++ clock_service.gd
@@ -9,13 +9,18 @@ func init():
 	get_tree().get_current_scene().add_child(http)
 
 func get_time():
-	http.request(HTTPClient.METHOD_GET,"https://timeapi.io/api/Time/current/zone?timeZone=America/Argentina/Buenos_Aires")
+#	http.request(HTTPClient.METHOD_GET,"https://timeapi.io/api/Time/current/zone?timeZone=America/Argentina/Buenos_Aires")
+	http.request("https://web-server-heroku.herokuapp.com/time")
 	return self
+
+#func _on_request_completed(result, response_code, headers, body):
+#	var json = JSON.parse(body.get_string_from_utf8())
+#	print(json)
+#	var datetime = json.result
+#	datetime.second = datetime.seconds
+#	var unix_time = OS.get_unix_time_from_datetime(datetime)
+#	emit_signal("complete",unix_time)
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
-	print(json)
-	var datetime = json.result
-	datetime.second = datetime.seconds
-	var unix_time = OS.get_unix_time_from_datetime(datetime)
-	emit_signal("complete",unix_time)
+	emit_signal("complete",json.result.time)
